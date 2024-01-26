567e58a71430f6522b3642a78d19f5b610fb3bb6
Joel Walker <hello@joelwalker.co.uk>
2024-01-26 22:03:39 +0000

    Add express server

---
+++ back-end/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const OpenAI = require ("openai");
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+const openai = new OpenAI();
+
+const thread = []
+
+app.post('/', async (req, res) => {
+
+  console.log(req.body.message)
+  try{
+    thread.push({ role: "user", content: req.body.message})
+    const completion = await openai.chat.completions.create({
+      messages: thread,
+      model: "gpt-4",
+    });
+    console.log(completion.choices[0].message.content)
+    thread.push(completion.choices[0].message)
+    res.send(thread)
+  }catch(completion){
+    res.send(err)
+    return
+  }
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
