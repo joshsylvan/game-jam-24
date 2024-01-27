463081ada4c8a79c95614bfd1b1cb729b3a4007d
Joel Walker <hello@joelwalker.co.uk>
2024-01-27 18:08:07 +0000

    Add try catch to api

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -12,13 +12,15 @@ app.use(express.json())
 const openai = new OpenAI();
 
 app.post('/sitcom', async (req, res) => {
- 
+  try{
     const { settingPrompt, characters } = req.body
     const script = await scriptGenerator(openai, settingPrompt, characters);
     const parsedScript = scriptParser(script, characters);
     //parsedScript.background_url = await imageGenerator(openai, parsedScript.scene);
     res.send(parsedScript)
- 
+  }catch(err){
+    res.status(500).send(err.message)
+  }
 })
 
 app.listen(port, () => {
